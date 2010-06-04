9f47f2a885963d5590cf627b9309b784fd0ba642
Toby <toby.tripp+git@gmail.com>
2010-06-04 10:03:43 -0500

    Don't try to clone the repo if it's already there.

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ Remote Server Installation
 --------------------------
 
 The repository contains a script for setting up some bare-bones
-dotfiles on remote servers.  These include things like screen
-configuration, simple bashrc, and the sort.
+dotfiles on remote servers.  These files include things like screen
+configuration, a simple bashrc, and the sort.
 
 Install with:
     bash < <( curl http://github.com/tobytripp/dotfiles/raw/master/remote-install.sh )
--- remote-install.sh
+++ remote-install.sh
@@ -1,14 +1,23 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # A script to install some bare-bones dotfiles for use on remote servers.
 # 
 # Usage:
 #  bash < <( curl http://github.com/tobytripp/dotfiles/raw/master/remote-install.sh )
 
-git clone http://github.com/tobytripp/dotfiles.git .dotfiles
-pushd .dotfiles
+DOTFILE_PATH="${DOTFILE_PATH:-$HOME/.dotfiles}"
+
+if [[ -d "$HOME/.dotfiles" ]]; then
+    pushd $DOTFILE_PATH
+    git pull origin master
+else
+    git clone http://github.com/tobytripp/dotfiles.git $DOTFILE_PATH
+    pushd $DOTFILE_PATH
+fi
 
 for dotfile in screenrc bashrc
 do
     ln -s $dotfile ~/.$dotfile
 done
+
+popd
