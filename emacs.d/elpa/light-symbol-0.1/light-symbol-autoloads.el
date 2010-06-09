3a3f98a95875c85a320c432276ee63488658338c
Sudhindra Rao[betarelease] <sudhindra.r.rao@gmail.com>
2010-06-09 10:05:00 -0500

    Sudhindra: removed old emacs files

--- emacs.d/elpa/light-symbol-0.1/light-symbol-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; light-symbol-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (light-symbol-mode) "light-symbol" "light-symbol.el"
-;;;;;;  (18741 38090))
-;;; Generated autoloads from light-symbol.el
-
-(autoload (quote light-symbol-mode) "light-symbol" "\
-Toggle Light Symbol mode.
-With no argument, this command toggles the mode.
-Non-null prefix argument turns on the mode.
-Null prefix argument turns off the mode.
-
-When Light Symbol mode is enabled, pausing will highlight the
-symbol under point in the current buffer.
-
-\(fn &optional ARG)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("light-symbol-pkg.el") (18741 38090 532368))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; light-symbol-autoloads.el ends here
