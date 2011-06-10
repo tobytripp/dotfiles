dd5a7861751059bb6e856473a32fa5c92bdc4d06
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2011-06-09 21:39:14 -0700

    auto screen added to my loader

---
+++ user_specific/sudhindra/auto_screen.sh
@@ -0,0 +1,16 @@
+# Auto-screen invocation. see: http://taint.org/wk/RemoteLoginAutoScreen
+# if we're coming from a remote SSH connection, in an interactive session
+# then automatically put us into a screen(1) session. Only try once
+# -- if $STARTED_SCREEN is set, don't try it again, to avoid looping
+# if screen fails for some reason.
+
+if [ "$PS1" != "" -a "${STARTED_SCREEN:-x}" = x -a "${SSH_TTY:-x}" != x ]
+then
+ STARTED_SCREEN=1 ; export STARTED_SCREEN
+ [ -d $HOME/lib/screen-logs ] || mkdir -p $HOME/lib/screen-logs
+ sleep 1
+ screen -RR && exit 0
+ 
+ # normally, execution of this rc script ends here...
+ echo "Screen failed! continuing with normal bash startup"
+fi
