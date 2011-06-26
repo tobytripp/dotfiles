1dab95b2fe73e4467172f7b4a00f55323d3404fc
Sudhindra Rao [betarelease] <sudhindra.r.rao@gmail.com>
2011-06-25 18:18:16 -0700

    Sudhindra: green color again for vcs information

--- bash_boost/profile_prompt.sh
+++ bash_boost/profile_prompt.sh
@@ -2,7 +2,7 @@ source ~/.bash_boost/bash_colors.sh
 
 function prompt {
     PROMPT_COMMAND=detect_vcs
-    PS1="$CYAN\u$NORMAL:\${__vcs_prefix}$CYAN\${base_dir}\[\$(check_git_changes)\]\${__vcs_branch_tag}$CYAN\${__cwd}$NORMAL \$ "
+    PS1="$CYAN\u$NORMAL:$GREEN\${__vcs_prefix}$CYAN\${base_dir}\[\$(check_git_changes)\]\${__vcs_branch_tag}$CYAN\${__cwd}$NORMAL \$ "
   # PS2='> '
   # PS4='+ '
 }
