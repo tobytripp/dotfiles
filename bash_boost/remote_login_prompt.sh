0952832d634c7c835c08b4f4131a201a34d64194
Sheroy <smarker@thoughtworks.com>
2013-04-22 21:54:39 -0700

    Revert "few more places where tput is called without checking term capability - fixed"
    This reverts commit 8b5e220c4bb83d3fc6960513bc5bbfc19259fd10.

--- bash_boost/remote_login_prompt.sh
+++ bash_boost/remote_login_prompt.sh
@@ -2,14 +2,7 @@ source ~/.bash_boost/bash_colors.sh
 
 function prompt {
     PROMPT_COMMAND=detect_vcs
-    REMOTE_LOGIN_PROMPT="$CYAN\u$NORMAL@$GREEN\h:$GREEN\${__vcs_prefix}$CYAN\${base_dir}\[\$(check_git_changes)\]\${__vcs_branch_tag}$CYAN\${__cwd}$NORMAL \$ "
-    NON_TTY_PROMPT='[\u@\h \W]\$ '
-    if [[ $- =~ i ]]
-    then
-      PS1=$REMOTE_LOGIN_PROMPT
-    else
-      PS1=$NON_TTY_PROMPT
-    fi    
+    PS1="$CYAN\u$NORMAL@$GREEN\h:$GREEN\${__vcs_prefix}$CYAN\${base_dir}\[\$(check_git_changes)\]\${__vcs_branch_tag}$CYAN\${__cwd}$NORMAL \$ "
   # PS2='> '
   # PS4='+ '
 }
