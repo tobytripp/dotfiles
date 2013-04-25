c5c23abe87ce59839cf969f6064f49d06f7ad5e1
Sheroy <smarker@thoughtworks.com>
2013-04-24 21:32:01 -0700

    using cat instead of echo because non-interactive shells like scp do not like stdout http://teaching.idallen.com/cst8207/13w/notes/350_startup_files.html#non-interactive-shell

--- bash_boost/profile_prompt.sh
+++ bash_boost/profile_prompt.sh
@@ -2,14 +2,7 @@ source ~/.bash_boost/bash_colors.sh
 
 function prompt {
     PROMPT_COMMAND=detect_vcs
-    INTERACTIVE_PROMPT="$GREEN\${__vcs_prefix}$CYAN\${base_dir}\[\$(check_git_changes)\]\${__vcs_branch_tag}$CYAN\${__cwd}$NORMAL \$ "
-    NON_TTY_PROMPT='[\u@\h \W]\$ '
-    if [[ $- =~ i ]]
-    then
-      PS1=$INTERACTIVE_PROMPT
-    else
-      PS1=$NON_TTY_PROMPT
-    fi
+    PS1="$GREEN\${__vcs_prefix}$CYAN\${base_dir}\[\$(check_git_changes)\]\${__vcs_branch_tag}$CYAN\${__cwd}$NORMAL \$ "
   # PS2='> '
   # PS4='+ '
 }
