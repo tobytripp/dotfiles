0c9d032d8bfbd732658450e683dd3130912009c9
Toby Tripp <toby.tripp+github@gmail.com>
2017-12-08 11:16:00 -0600

    Leinengen profiles

---
+++ lein/profiles.d/perf.clj
@@ -0,0 +1,5 @@
+{:jvm-opts ^:replace ["-server"
+                      "-Xmx4096m"
+                      "-Dclojure.compiler.direct-linking=true"]
+ :dependencies [[criterium "0.4.4"]]}
+
--- lein/profiles.d/user.clj
+++ lein/profiles.d/user.clj
@@ -1,4 +1,5 @@
 {:plugins [[lein-ancient "0.6.14"]
            [jonase/eastwood "0.2.4"]    ; clojure linter
            ]}
-
+{:eastwood {:namespaces [:source-paths]
+            :add-linters [:unused-namespaces]}}
